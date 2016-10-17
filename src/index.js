79fd33c02bd0b0f2c580ffc443d96b2d0f50854e
Jeff <jeffhtli@163.com>
2016-10-17 22:23:21 +0800

    add dev dependency 'react-scripts'

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ import reducers from './reducers'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-//let store = createStore(reducers);
-
+//const store = createStore(reducers);
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDom.render(
   <Provider store={store}>
     <CartApp />
   </Provider>,
-  document.getElementById('container')
+  document.getElementById('root')
 );
